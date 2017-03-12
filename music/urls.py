1f6f76449c6fa5e0d697efe35b1e9de891bedb81
Chris Starling <cstarling@gmail.com>
2017-03-11 19:40:59 -0500

    stuff

--- music/urls.py
+++ music/urls.py
@@ -11,11 +11,9 @@ urlpatterns = [
 
 
    # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     #music/712
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name='detail'),
 
-
-    url(r'^(?P<album_id>[0-9]+)/favorite$', views.favorite, name='favorite'),
 ]
